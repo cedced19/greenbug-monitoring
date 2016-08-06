f10312cc50b1a9f3d9b5eb743b98673f7a939443
Cédric <cedced19@gmail.com>
2016-08-06 11:30:35 +0200

    Add check if configuration exists before running the app

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # greenbug-monitoring
 A software to send informations of a server to a Greenbug server. The aim is to do some monitoring.
+
+## Configuration example
+
+```json
+{
+  "url": "https://example-adress.com/",
+  "password": "1FkMtEBySLPxcDL1u3CwexxaEc9lFwiWbUOExhSq8hFjmf26YHh0771BOFghXVpj"
+}
+```
+_This content will be automatically given by the Web UI of Greenbug._
+
+## Interaction with Greenbug Server
+![Interaction with Greenbug Server](interaction-with-server.png)
--- greenbug-monitoring.js
+++ greenbug-monitoring.js
@@ -3,13 +3,40 @@ var path = require('path');
 var existsFile = require('exists-file');
 var log = require('captains-log')();
 
-var tokenStorage = require('token-storage');
+var configFile = path.join(__dirname, '/config.json');
+
+// Check if config.json exists
+if (!existsFile.sync(configFile)) {
+  log.error('No config.json file. Please create it and fill it with the url of the Greenbug server and the password.');
+  return process.exit(1);
+}
+
+var config = require(configFile);
+
+// Check for password
+if (typeof config.password == 'undefined' || config.password === '') {
+  log.error('No password defined.');
+  return process.exit(1);
+}
+
+// Check for url
+if (typeof config.url == 'undefined' || config.url === '' || !require('is-url')(config.url)) {
+  log.error('No valid url defined.');
+  return process.exit(1);
+}
 
 var cachePath = path.join(__dirname, '/.greenbug-cache');
 
+// Check if cache directory exists
 if (!existsFile.sync(cachePath)) {
-  log.info('No cache directory, create it.');
+  log.info('No cache directory. Creating it...');
   fs.mkdirSync(cachePath);
 }
 
-if ()
+var tokenStorage = require('./lib/token-storage');
+var payload = tokenStorage.getPayload();
+
+// Check if jwt is valid
+if (!payload || payload.exp <= (Date.now() - 86400000)) {
+  log.info('No valid json web token, trying to get it from server.');
+}
